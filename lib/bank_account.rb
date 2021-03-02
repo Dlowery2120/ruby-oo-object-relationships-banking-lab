ad98601222216efeaf921017b00f266ca58504bf
Devante Lowery <dlowery2120@gmail.com>
2021-03-02 17:51:45 -0600

    commit tree d7c9b7c5d855cccfd5b24310be35324c70d46d8d

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,38 @@
-class BankAccount
 
+class BankAccount
+    attr_accessor :balance, :status
+    attr_reader :name
+
+    @@all = []
+
+    def initialize (name)
+        @name = name
+        @balance = 1000
+        @status = "open"
+        @@all << self
+    end
+
+    def deposit(deposit_amount)
+        self.balance += deposit_amount
+    end
+
+    def display_balance
+        "Your balance is $#{self.balance}."
+    end
+
+    def valid?
+        if self.balance > 0 && self.status == "open"
+            true
+        else 
+            false
+        end
+    end
+
+    def close_account
+        self.status = "closed"
+    end
+
+    def self.all
+        @@all
+    end
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,81 @@
+require 'pry'
+
 class Transfer
-  # your code here
+
+  attr_accessor :sender, :receiver, :status, :amount
+
+  @@all = []
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+    @amount = amount
+    @@all << self
+  end
+
+  def valid?
+    if receiver.valid? && sender.valid? == true
+      true
+    else
+      false
+    end
+
+  end
+
+  def execute_transaction
+    if receiver.valid? && sender.balance > amount && receiver.balance && @status == "pending"
+      sender.balance -= amount
+      receiver.balance += amount
+      @status = "complete"
+    elsif sender.balance < amount
+      
+      @status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    elsif receiver.valid? == false
+      "Transaction rejected. Please check your account balance."
+      @status = "rejected"
+      "Transaction rejected. Please check your account balance."
+
+    end
+  end
+
+
+
+  def reverse_transfer
+    execute_transaction
+    sender.balance += amount
+    receiver.balance -= amount
+    self.status = "reversed"
+    # if receiver.valid? && sender.balance > amount && receiver.balance && @status == "pending"
+    #   sender.balance -= amount
+    #   receiver.balance += amount
+    #   @status = "complete"
+    # elsif sender.balance < amount 
+    #   sender.balance += amount
+    #   receiver.balance -= amount
+      
+    #   @status = "rejected"
+    #   "Transaction rejected. Please check your account balance."
+    # elsif receiver.valid? == false
+    #   "Transaction rejected. Please check your account balance."
+    #   @status = "rejected"
+    #   "Transaction rejected. Please check your account balance."
+    # end
+  end
+  # self.status = rejected
+
+  def self.all
+    @@all
+  end
 end
+b1 = BankAccount.new("Devante")
+b2 = BankAccount.new("Alex")
+b3 = BankAccount.new("Andre")
+
+t1 = Transfer.new(b1, b2, 25)
+t2 = Transfer.new(b3, b2, 30)
+t3 = Transfer.new(b2, b3, 50)
+
+# binding.pry
+# 0
